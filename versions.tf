bbd49f8345263f1957b3b5294539bba6419ce743
tabbertj <101358806+tabbertj@users.noreply.github.com>
2023-12-15 09:02:42 -0500

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -17,3 +17,8 @@ terraform {
     }
   }
 }
+
+provider "azurerm" {
+  skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  features {}
+}
